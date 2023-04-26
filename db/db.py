dc01db1e450c52d2fd5c168742418930aff6964a
prajeshElEvEn <prajesh.eleven118@gmail.com>
2023-04-26 19:58:24 +0530

    - crud in db

--- db/db.py
+++ db/db.py
@@ -9,7 +9,50 @@ firebase_admin.initialize_app(cred, {
     'databaseURL': 'https://auto-bot-2559f-default-rtdb.asia-southeast1.firebasedatabase.app'
 })
 
+# add data to firebase
 userRef = db.reference('Users/')
 userRef.set({
-    'name': 'Pranjal',
+    '1': {
+        'name': {
+            'firstName': 'Pranjal',
+            'lastName': 'Agarwal',
+        },
+        'phone': '8791224496',
+    },
+    '2': {
+        'name': {
+            'firstName': 'Prajesh',
+            'lastName': 'Singh',
+        },
+        'phone': '6393318060',
+    },
 })
+
+driverRef = db.reference('Drivers/')
+driverRef.set({
+    '1': {
+        'name': 'Agarwal',
+        'phone': '8791224496',
+        'vehicleNumber': 'DL 1C 1234',
+    },
+    '2': {
+        'name': 'Singh',
+        'phone': '6393318060',
+        'vehicleNumber': 'DL 1C 1235',
+    },
+})
+
+# update data to firebase
+toRef = userRef.child('2').child('name')
+toRef.update({
+    'lastName': 'Pratap Singh',
+})
+
+# delete data from firebase
+# toRef = userRef.child('2')
+# toRef.delete()
+
+# read data from firebase
+fromRef = userRef.child('1')
+fromData = fromRef.get()
+print(fromData)
