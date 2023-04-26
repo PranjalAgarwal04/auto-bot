76660a9ede7effdab1a7b9bae69e9f58b921d857
PranjalAgarwal04 <agarwalpranjal813@gmail.com>
2023-04-26 18:03:58 +0530

    changes

--- dialogues/userDialogues.py
+++ dialogues/userDialogues.py
@@ -1,8 +1,24 @@
 query = input("> ")
-query = query.lower
+query = query.lower()
+print(query)
 
+if (query == "hello" or query == "hi" or query == "hey"):
+    print("""Hello, do you want to book an auto?
+      1.Book an auto
+      2.Report
+      3.Help""")
+    query = input("> ")
 
-if(query == "hello" or query == "hi" or query == "hey"):
-   print("""
-      Hello, do you want to book a cab?
-   """)
+    if (query == "1"):
+        print("""Enter your current location.""")
+        query = input("> ")
+
+        print("""
+         1.Source loaction one
+         2.Source location two
+         3.Source location two
+         4.Refresh options""")
+        query = input("> ")
+
+        if (query == "1"):
+            print()
