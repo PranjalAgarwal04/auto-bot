187152f27b73d30ff2cb6a605e9797aad6a5e185
PranjalAgarwal04 <agarwalpranjal813@gmail.com>
2023-04-26 18:19:30 +0530

    changes

--- dialogues/userDialogues.py
+++ dialogues/userDialogues.py
@@ -14,11 +14,34 @@ if (query == "hello" or query == "hi" or query == "hey"):
         query = input("> ")
 
         print("""
-         1.Source loaction one
+         1.Source location one
          2.Source location two
-         3.Source location two
+         3.Source location three
          4.Refresh options""")
         query = input("> ")
 
-        if (query == "1"):
-            print()
+        for x in query:
+            if (x == "1"):
+                print("Source loaction one")
+                break
+            elif (x == "2"):
+                print("Source location two")
+                break
+            elif (x == "3"):
+                print("Source location three")
+                break
+            elif (x == "4"):
+                print("""
+                    1-Source location four
+                    2-Source location five
+                    3-Source location six
+               """)
+
+            query = input("> ")
+            print("Looking for the nearest auto driver...")
+            print("""
+                  Auto Located - 
+                  Driver name - 
+                  Auto number - 
+                  Contact number - 
+                  """)
