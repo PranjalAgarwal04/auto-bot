9011b1035ab69e9ce8b0c18de3a2b855e187f412
prajeshElEvEn <prajesh.eleven118@gmail.com>
2023-04-26 19:20:01 +0530

    added db

---
+++ db/db.py
@@ -0,0 +1,6 @@
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import db
+
+cred = credentials.Certificate(
+    'auto-bot-2559f-firebase-adminsdk-emjq1-43937ff21c.json')
