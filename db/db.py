148e0a7ec8840525da2f98c74bebb13ae8320289
prajeshElEvEn <prajesh.eleven118@gmail.com>
2023-04-26 19:27:23 +0530

    - added data to db

--- db/db.py
+++ db/db.py
@@ -4,3 +4,12 @@ from firebase_admin import db
 
 cred = credentials.Certificate(
     'auto-bot-2559f-firebase-adminsdk-emjq1-43937ff21c.json')
+
+firebase_admin.initialize_app(cred, {
+    'databaseURL': 'https://auto-bot-2559f-default-rtdb.asia-southeast1.firebasedatabase.app'
+})
+
+userRef = db.reference('Users/')
+userRef.set({
+    'name': 'Pranjal',
+})
